8f998f1468eebc6ab136ba4824274781789749bd
Guib999 <laroucheg12@gmail.com>
2025-04-26 13:26:40 -0400

    landing page finish

--- client/enviroactions/src/App.tsx
+++ client/enviroactions/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/LandingPage.tsx";
 
 function App() {
   const [count, setCount] = useState(0)
