bd99b1bd91f39820d96f1662cdc3ae1d1a89b1b4
SkweesheeEmile <2359794@johnabbottcollege.net>
2025-04-26 16:19:55 -0400

    fix

--- client/enviroactions/src/App.tsx
+++ client/enviroactions/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
 import LandingPage from "./components/LandingPage.tsx";
 import { Route, Routes } from "react-router-dom";
-import { SignIn } from './components/SignIn.tsx';
+import  { Account } from './components/SignIn.tsx';
 import { SignUp } from './components/SignUp.tsx';
-import { AccountInfo } from './components/AccountInfo.tsx';
-import { Home } from './components/Home.tsx';
+import  AccountInfo  from './components/AccountInfo.tsx';
+import Home from './components/Home.tsx';
 import { ReportPage } from './components/ReportPage.tsx';
 
 
@@ -12,7 +12,7 @@ function App() {
   return(
     <Routes>
     <Route path="/" element={<LandingPage />} />
-    <Route path="/signIn" element={<SignIn/>} />
+    <Route path="/signIn" element={<AccountInfo/>} />
     <Route path="/SignUp" element={<SignUp/>} />
     <Route path="/infoForm" element={<AccountInfo />} />
     <Route path="/home" element={<Home />} />
--- client/enviroactions/src/main.tsx
+++ client/enviroactions/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById("root")!).render(
         <BrowserRouter>
