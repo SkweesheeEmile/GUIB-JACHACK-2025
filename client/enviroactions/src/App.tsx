dc82714904669e762fd884cd7a5a500a433c4bd6
Guib999 <laroucheg12@gmail.com>
2025-04-26 14:06:57 -0400

    footer lien dans app

--- client/enviroactions/src/App.tsx
+++ client/enviroactions/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LandingPage from "./components/LandingPage.tsx";
+import Footer from './components/footer.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
