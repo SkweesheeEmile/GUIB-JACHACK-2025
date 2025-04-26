ec2292879292d30d1f401b56d28b7e092df0720a
SkweesheeEmile <2359794@johnabbottcollege.net>
2025-04-26 14:35:01 -0400

    routes

---
+++ client/enviroactions/src/components/AccountInfo.tsx
@@ -0,0 +1,8 @@
+export function AccountInfo() {
+    return (
+        <div>
+            <h1>Account Info</h1>
+            <p>Here you can view and edit your account information.</p>
+        </div>
+    );
+}
