ce2933b90ec891d8648b267078b01f7781d5d4cb
Alec Lyshtook <alystukha@gmail.com>
2025-04-27 05:14:39 -0400

    made style work for now

--- client/enviroactions/src/App.tsx
+++ client/enviroactions/src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage.tsx";
 import { Route, Routes } from "react-router-dom";
-import  GetInfo  from './components/AccountInfo.tsx';
-import  Home  from './components/Home.tsx';
-import  ReportPage from './components/ReportPage.tsx';
+import Home from "./components/home.tsx";
+import ReportPage from "./components/ReportPage.tsx";
 import Forms from "./components/Forms.tsx";
+import Footer from "./components/footer.tsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/infoForm" element={<GetInfo />} />
-      <Route path="/form" element={<Forms/>}></Route>
-      <Route path="/home" element={<Home />} />
-      <Route path="/report" element={<ReportPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/form" element={<Forms />}></Route>
+        <Route path="/home" element={<Home />} />
+        <Route path="/report" element={<ReportPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- client/enviroactions/src/components/LandingPage.tsx
+++ client/enviroactions/src/components/LandingPage.tsx
@@ -1,8 +1,22 @@
 // import { useNavigate } from 'react-router-dom';
 import AuthButtons from "./AuthButtons";
 import Footer from "./footer";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const { isAuthenticated, isLoading } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      // If user is authenticated and the app is done loading, redirect
+
+      navigate("/home"); // <-- your destination page
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
   return (
     <>
       <div className="full-width" id="navBar">
@@ -22,7 +36,6 @@ function LandingPage() {
           goals.
         </h3>
       </div>
-      <Footer />
     </>
   );
 }
